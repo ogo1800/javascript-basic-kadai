38db5b346d28533d4fb83126d5cfd303911969c5
ogo1800 <r.ogo@cellpromote.biz>
2023-08-27 22:06:03 +0900

    fixed

--- kadai_013/square.js
+++ kadai_013/square.js
@@ -1,7 +1,7 @@
 // 引数の2乗が出力される関数を定義する
 const calculateSquare = (num) => {
-    console.log(num * num);
+    return num * num;
 }
 
 // 関数を呼び出し、引数として10を渡す
-calculateSquare(10);
+console.log(calculateSquare(10));
